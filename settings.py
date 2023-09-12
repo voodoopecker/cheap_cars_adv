929ab95c7a3f861ab1b6dd5cfbd08bdc2291ab6e
xubuntu <xubuntu@voodoopecker.xu>
2023-09-12 15:03:52 +0300

    settings.py: settings constants moved to .env

--- settings.py
+++ settings.py
@@ -4,10 +4,21 @@ ADMIN_ID - telegram account ID who controls bot.
 GROUP_ID - telegram group ID where posts send.
 MAIN_LINK - source page link from where data scraps.
 SCHEDULER_TIME - interval between runs in seconds
-"""
 
-TOKEN: str = ''
-ADMIN_ID: int = 0
-GROUP_ID: int = 0
-MAIN_LINK: str = ''
+Create .env file in the same folder with all these constants filled with your private data (example):
+TOKEN: str = '3452345344:DSA5-dfgKd3JAoQ7rj2D3NVRoQ0Qs-zx5Yp'
+ADMIN_ID: int = 6786786734
+GROUP_ID: int = -1007638736412
+MAIN_LINK: str = 'https://XautoX.ru/moscow/cars'
+"""
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+TOKEN: str = str(os.environ.get('TOKEN'))
+ADMIN_ID: int = int(os.environ.get('ADMIN_ID'))
+GROUP_ID: int = int(os.environ.get('GROUP_ID'))
+MAIN_LINK: str = str(os.environ.get('MAIN_LINK'))
+
 SCHEDULER_TIME: int = 3600
